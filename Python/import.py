1fe144cc30ddb0075ae0b9dba991a6787008698f
siddharth.singh <siddharth.singh@thalesgroup.com>
2026-05-24 00:23:08 +0530

    python learning

---
+++ Python/import.py
@@ -0,0 +1,42 @@
+# %% [markdown]
+# Imports in python
+
+# %%
+import math
+math.sqrt(16)
+
+# %%
+from math import sqrt,pi
+print(sqrt(16))
+print(sqrt(9))
+print(pi)
+
+# %%
+import numpy as np
+np.array([1,2,3,4])
+
+
+# %%
+import sys
+print(sys.executable)
+
+# %%
+import sys
+!"{sys.executable}" -m pip install numpy
+
+# %%
+from math import *
+print(pi)
+
+# %%
+from package.maths import addition
+addition(2,3)
+
+# %%
+from package import maths
+maths.addition(5,6)
+
+# %%
+
+
+
